468e977d45c831b1bb4fd460f98fc6be6b6911a7
root <root@LAPTOP-GRRJEDSG.localdomain>
2023-07-16 11:21:35 +0200

    WIP: created .c file with main function.

---
+++ insterstellar_pong.c
@@ -0,0 +1,17 @@
+/**
+ * @file interstellar_pong.c
+ * @author Marek Eibel
+ * @brief InterStellar Pong is a game (...).
+ * @version 0.1
+ * @date 2023-07-16
+ * 
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#include <stdio.h>
+int main() 
+{
+    printf("%s\n", "Hello world!");
+    return 0;
+}
